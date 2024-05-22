eff40dda28f26ac4c30a9e05cf9ca8feaa2553f3
ravenstorm43 <201912065@office.induk.ac.kr>
2024-05-22 15:06:49 +0900

    쿠키와 세션 실습

--- src/test/java/com/sparta/springauth/BeanTest.java
+++ src/test/java/com/sparta/springauth/BeanTest.java
@@ -12,10 +12,10 @@ public class BeanTest {
     @Autowired
     @Qualifier("pizza")
     Food food;
-//    @Autowired
-//    Food chicken; // 같은 타입의 bean객체가 두개 이상일때 autowire하는법
-//    @Autowired
-//    Food pizza; // 첫째: 등록된 bean이름 명시
+    @Autowired
+    Food chicken; // 같은 타입의 bean객체가 두개 이상일때 autowire하는법
+    @Autowired
+    Food pizza; // 첫째: 등록된 bean이름 명시
 
     @Test
     @DisplayName("테스트")
